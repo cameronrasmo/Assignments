5435ba334017c39db80ba1598b73b121cc6b910f
Cameron Rasmussen <cameron.p.rasmussen@gmail.com>
2020-02-04 17:14:39 -0700

    Module 0 Filename

--- Module 0/Grocery Store/app.js
+++ Module 0/Grocery Store/app.js
@@ -2,7 +2,7 @@ var shopper = {
     fName: "Cameron",
     lName: "Rasmussen",
     age: 12,
-    groceryCart: ["Apples", "T-Shirt", "Laundry Detergent"]
+    groceryCart: ["Apples", "T-Shirt", "Laundry Detergent"],
     greeting: function(){
         console.log("Hello! My name is " + this.fName + " " + this.lName + " and I'm " + this.age + " years old.")
     }
