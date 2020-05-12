fa1d05205f47922793a8dc797625493680996569
Cameron Rasmussen <cameron.p.rasmussen@gmail.com>
2020-05-12 16:22:01 -0600

    full post functionality, working on connecting comment schema

--- Module 6/RTV/routes/postRouter.js
+++ Module 6/RTV/routes/postRouter.js
@@ -3,6 +3,7 @@ const Post = require("../models/post.js");
 
 const postRouter = express.Router();
 
+// Get all, and new post
 postRouter
     .route("/")
     .get((req, res, next) => {
@@ -26,6 +27,7 @@ postRouter
         });
     });
 
+// Get by author ID
 postRouter.route("/:authorID").get((req, res, next) => {
     Post.findOne({ author: req.params.authorID }, (err, found) => {
         if (err) {
@@ -34,6 +36,63 @@ postRouter.route("/:authorID").get((req, res, next) => {
         }
         res.status(200).send(found);
     });
+});
+
+// Update post, delete post
+postRouter
+    .route("/:postID")
+    .put((req, res, next) => {
+        Post.findOneAndUpdate(
+            { _id: req.params.postID },
+            req.body,
+            { new: true },
+            (err, updated) => {
+                if (err) {
+                    res.status(500);
+                    return next(err);
+                }
+                res.status(201).send(updated);
+            }
+        );
+    })
+    .delete((req, res, next) => {
+        Post.findOneAndDelete({ _id: req.params.postID }, (err, deleted) => {
+            if (err) {
+                res.status(500);
+                return next(err);
+            }
+            res.status(200).send(deleted);
+        });
+    });
+
+postRouter.route("/upvote/:postID").put((req, res, next) => {
+    Post.findOneAndUpdate(
+        { _id: req.params.postID },
+        { $inc: { upvotes: 1 } },
+        { new: true },
+        (err, updated) => {
+            if (err) {
+                res.status(500);
+                return next(err);
+            }
+            res.status(201).send(updated);
+        }
+    );
+});
+
+postRouter.route("/downvote/:postID").put((req, res, next) => {
+    Post.findOneAndUpdate(
+        { _id: req.params.postID },
+        { $inc: { downvotes: 1 } },
+        { new: true },
+        (err, updated) => {
+            if (err) {
+                res.status(500);
+                return next(err);
+            }
+            res.status(201).send(updated);
+        }
+    );
 });
 
 module.exports = postRouter;
